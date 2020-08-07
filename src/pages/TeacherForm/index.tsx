db3500c09f3063b391841c6635f649bbe591cad4
sensui <sensuijunior@hotmail.com>
2020-08-06 21:48:36 -0300

    PageHeader

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+function TeacheForm() {
+  return (
+    <div id='page-teacher-form' className='container'>
+      <PageHeader title='Que incrivel que você quer dar aulas.' />
+    </div>
+  );
+}
+
+export default TeacheForm;
